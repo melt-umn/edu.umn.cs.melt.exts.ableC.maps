78a79fda4927ade4e1b2d264031fe6dcef5a847d
Nathaniel Ringo <remexre@gmail.com>
2017-01-17 12:14:31 -0600

    Add const, pointer, etc. support to map type.

--- src/concretesyntax/types.sv
+++ src/concretesyntax/types.sv
@@ -7,7 +7,7 @@ imports edu:umn:cs:melt:exts:ableC:maps:src:abstractsyntax as abs;
 terminal Map_t 'map' lexer classes {Ckeyword};
 
 concrete productions top::TypeSpecifier_c
-| Map_t LessThan_t TypeSpecifier_c Comma_t TypeSpecifier_c GreaterThan_t
+| Map_t LessThan_t TypeName_c Comma_t TypeName_c GreaterThan_t
 {
   top.realTypeSpecifiers = [errorTypeExpr([])];
   top.preTypeSpecifiers = [];
