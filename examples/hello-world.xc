bce1aa1b873bdff368106a4b0d7fffa3c0fc9ecf
Nathaniel Ringo <remexre@gmail.com>
2017-01-17 13:20:38 -0600

    The start of MapExprList_c.

--- examples/hello-world.xc
+++ examples/hello-world.xc
@@ -2,9 +2,9 @@
 
 int main(int argc, char **argv) {
     map<int, const char*> m = new_map<int, const char*> {
-        "OK",
-        "Hello",
-        "why this?"
+        1: "OK",
+        2: "Hello",
+        5: "why this?"
     };
     printf("%s World.\n", m[5]);
     return 0;
--- src/concretesyntax/constructor/Constructor.sv
+++ src/concretesyntax/constructor/Constructor.sv
@@ -12,5 +12,17 @@ import edu:umn:cs:melt:exts:ableC:maps:src:abstractsyntax;
 marking terminal NewMap_t 'new_map' lexer classes {Ckeyword};
 
 concrete productions top::PrimaryExpr_c
-| 'new_map' '<' k::TypeName_c ',' v::TypeName_c '>' '{' elems::ArgumentExprList_c '}'
+| 'new_map' '<' k::TypeName_c ',' v::TypeName_c '>' '{' elems::MapExprList_c '}'
   { top.ast = constructMap(k.ast, v.ast, foldExpr(elems.ast), location=top.location); }
+
+closed nonterminal MapExprList_c with location, ast<[Pair<Expr Expr>]>;
+concrete productions top::MapExprList_c
+| e::AssignExprPair_c
+  { top.ast = [e.ast]; }
+| h::MapExprList_c ',' t::AssignExprPair_c
+  { top.ast = h ++ [t]; }
+
+closed nonterminal AssignExprPair_c with location, ast<Pair<Expr Expr>>;
+concrete productions top::AssignExprPair_c
+| k::AssignExpr_c ':' v::AssignExpr_c
+  { top.ast = pair(k.ast, v.ast); }
